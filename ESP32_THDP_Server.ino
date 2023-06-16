540b03eaa9a0022844b1c0875563e3853f302375
CF20852 <CF20852@users.noreply.github.com>
2023-06-16 16:04:05 -0700

    Update ESP32_THDP_Server.ino

--- ESP32_THDP_Server.ino
+++ ESP32_THDP_Server.ino
@@ -1,3 +1,23 @@
+/*Copyright (c) 2023 Chip Fleming
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.*/
+  
 #include <WiFi.h>
 #include <SparkFunBME280.h>
 #include <AsyncTCP.h>
@@ -57,7 +77,7 @@ void setupWiFi(void) {
 }
 
 // Set up things to use the hardware timer alarm feature to signal time to take a sample
-// We’ll have a timer increment every microsecond, so 180*10^6 increments = 180 seconds = 3 minutes
+// Weâ€™ll have a timer increment every microsecond, so 180*10^6 increments = 180 seconds = 3 minutes
 hw_timer_t * timer0 = NULL;
 const uint64_t alarmTimeout = 180UL * 1000000UL;
  
